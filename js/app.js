31838109743b8dd0b0347194fc14cf25a4f91f62
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-26 11:22:54 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        return getPin()
+    }
+}
+
+function getGeneratePin() {
+    const pin = getPin();
+    const displayPin = document.getElementById('display-pin');
+
+}
+
+
+
+
+// https://web.programming-hero.com/web-4/video/web-4-27-6-use-event-bubble-to-create-calculator-and-clear
+
